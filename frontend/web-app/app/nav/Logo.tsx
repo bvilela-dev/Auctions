e20fccc3469dfb70ced30509a2f8ebec7c2964f6
Bruno Vilela <bruno.vilela.magalhaes@gmail.com>
2025-09-17 18:39:10 -0300

    Chaged logo

--- frontend/web-app/app/nav/Logo.tsx
+++ frontend/web-app/app/nav/Logo.tsx
@@ -9,7 +9,7 @@ export default function Logo() {
     return (
         <div onClick={reset} className="cursor-pointer flex items-center gap-2 text-3xl font-semibold text-red-500">
             <AiOutlineCar size={34} />
-            <div>Carsties Auctions</div>
+            <div>Auctions</div>
         </div>
     )
 }
--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -32,7 +32,7 @@ export default function Search() {
                 onChange={handleChange}
                 value={value}
                 type="text"
-                placeholder="Search for cars by manufacturer, model or color"
+                placeholder="Search for itens by manufacturer, model or color"
                 className="
                 flex-grow
                 pl-5
